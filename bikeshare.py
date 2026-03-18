ffdae466efcbc2a0f0d986db977619d1c3aaafee
Rinor Arifi <rinor.arifi@cariad.technology>
2026-03-18 11:57:07 +0100

    refactor: add comments to improve readability

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+# Refactoring: added comments to improve readability
+
 CITY_DATA = {
     'chicago': 'chicago.csv',
     'new york city': 'new_york_city.csv',
